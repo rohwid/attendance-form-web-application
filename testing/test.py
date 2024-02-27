d012558718d0ad31df630605ef0ebc42d5d8f692
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2024-02-27 21:34:19 +0700

    ci/cd pipeline fixed and tested

--- testing/test.py
+++ testing/test.py
@@ -18,5 +18,5 @@ def get_title_from_web(url):
 web_url = "http://localhost:5002"
 title = get_title_from_web(web_url)
 
-assert title == "Form Absensi Mahasiswa (Test)"
+assert title == "Form Absensi Mahasiswa"
 print("Testing success")
