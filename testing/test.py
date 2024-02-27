ded8512685186512aa63a3b8225a32158bcffad7
Rohman Widiyanto <widiyanto.rohman@gmail.com>
2024-02-27 22:35:39 +0700

    rename apps as new feature

--- testing/test.py
+++ testing/test.py
@@ -18,5 +18,5 @@ def get_title_from_web(url):
 web_url = "http://localhost:5002"
 title = get_title_from_web(web_url)
 
-assert title == "Form Absensi Mahasiswa"
+assert title == "Form Absensi Pacmann University"
 print("Testing success")
